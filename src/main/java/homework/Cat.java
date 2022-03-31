30431ebd3647f94e5bf9e6e7edbbedd37c1a1670
KolesnichenkoAG <kag.1981@mail.ru>
2022-04-01 00:17:10 +0600

    итог на вечер 31.03.22

--- src/main/java/homework/Cat.java
+++ src/main/java/homework/Cat.java
@@ -3,15 +3,34 @@ package homework;
 public class Cat {
     private String name;
     private int appetite;
+    private int fullness;
+    int startfullness;
 
-    public Cat(String name, int appetite) {
+    public Cat(String name, int appetite, int fullness) {
         this.name = name;
         this.appetite = appetite;
+        this.fullness = fullness;
     }
 
     public void eat(Plate plate) {
-        System.out.println("Кот начал есть, съел " + appetite);
-        plate.decreaseFood(appetite);
+        if (plate.check(appetite) == true && startfullness < fullness) {
+            System.out.println("Кот " + name + " начал есть, съел " + appetite);
+            plate.decreaseFood(appetite);
+            startfullness = (startfullness + appetite);
+            satiety();
+        }
+        if (plate.check(appetite) == false){
+            System.out.println("Коту " + name + " нечего есть ");
+        }
+        if (startfullness < fullness && plate.check(appetite) == false) {
+            System.out.println("Кот " + name + " не наелся ");
+        }
     }
-
+    public boolean satiety() {
+        if (startfullness >= fullness) {
+            System.out.println("Кот " + name + " наелся он съел " + fullness);
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/homework/Plate.java
+++ src/main/java/homework/Plate.java
@@ -2,16 +2,25 @@ package homework;
 
 public class Plate {
     private int foodQuantity;
+    private int minFood;
 
-    public Plate(int foodQuantity) {
+    public Plate(int foodQuantity, int minFood) {
         this.foodQuantity = foodQuantity;
+        this.minFood = minFood;
     }
 
     public void info() {
         System.out.println("Текущее кол-во еды " + foodQuantity);
     }
-
     public void decreaseFood(int foodQuantity) {
-        this.foodQuantity -= foodQuantity;
+            if (this.foodQuantity > minFood) {
+                this.foodQuantity -= foodQuantity;
+            }
+    }
+    public boolean check(int minFood) {
+        if (this.foodQuantity >= minFood) {
+            return true;
+        }
+        return false;
     }
 }
